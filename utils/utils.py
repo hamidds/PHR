a13f1e42edde04e3363dc8576efb1718d4f1a669
Mohammad Hashemi <mohammad99hashemi@gmail.com>
2021-08-18 20:46:11 +0430

    feat: resize_padding function added

--- utils/utils.py
+++ utils/utils.py
@@ -1,47 +1,32 @@
-import os
-import num2word
-import cv2
-import random
+from PIL import Image
+from torchvision.transforms import ToTensor, Normalize
 
-words_path = "../data/words/"
+mean=[0.485, 0.456, 0.406]
+std=[0.229, 0.224, 0.225]
 
-def hconcat_resize(img_list):
+def resize_padding(img, width, height):
+	'''
 
-	# take minimum height
-	h_max = max(img.shape[0] for img in img_list)
+	:param img: input image
+	:param width: desired width size
+	:param height: desired height size
+	:return: image with the new size
+	'''
 
-	white=[255,255,255]
-	img_list_resize = [cv2.copyMakeBorder(img, int((h_max-img.shape[0])/2), h_max-img.shape[0]-int((h_max-img.shape[0])/2), 0, 0, cv2.BORDER_CONSTANT, value=white)
-					   for img in img_list]
+	desiredW, desiredH = width, height
+	imgW, imgH = img.size
+	ratio = 1.0 * imgW/imgH
+	newW = int(desiredH * ratio)
+	newW = newW if desiredW == None else min(desiredW, newW)
+	img = img.resize((newW, desiredH), Image.ANTIALIAS)
 
-	# return final image
-	return cv2.hconcat(img_list_resize)
+	# padding image
+	if desiredW != None and desiredW > newW:
+		new_img = Image.new("RGB", (desiredW, desiredH), color=255)
+		new_img.paste(img, (0, 0))
+		img = new_img
 
-def display_num_image(num_string):
+	img = ToTensor()(img)
+	img = Normalize(mean, std)(img)
 
-	words = num_string.split(" ")
-	word_img_list = []
-	for word in words:
-
-		image_dir_path = words_path + word + "/"
-		all_images_in_directory = os.listdir(image_dir_path)
-		# choose a random word image from the related directory
-		random_word_image_path = random.choice(all_images_in_directory)
-		word_img = cv2.imread(image_dir_path + random_word_image_path)
-		# concat word images horizontally
-		word_img_list.insert(0, word_img)
-
-	num_string_image = hconcat_resize(word_img_list)
-	cv2.imshow('test.jpg', num_string_image)
-	cv2.waitKey()
-
-
-
-if __name__ == '__main__':
-
-	random_num = 123120000
-	num_string = num2word.convert(random_num)
-	num_string += " ریال"
-
-	print(num_string)
-	display_num_image(num_string)
+	return img
